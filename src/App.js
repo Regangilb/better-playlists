a77d4074d9602cb802a88d39b652cbb2997bbf76
Regan Gilbert <regan.m.gilbert@gmail.com>
2018-07-18 16:18:02 -0600

    added song search functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import queryString from 'query-string';
 
-
 let defaultStyle = {
   color: '#fff'
 };
 let fakeServerData = {
   user: {
-    name: 'Regan',
+    name: 'David',
     playlists: [
       {
         name: 'My favorites',
@@ -65,7 +64,7 @@ class Playlist extends Component {
     let playlist = this.props.playlist
     return (
       <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
-      <img src={playlist.imageUrl} style={{width: '60px'}}/>
+        <img src={playlist.imageUrl} style={{width: '60px'}}/>
         <h3>{playlist.name}</h3>
         <ul>
           {playlist.songs.map(song => 
@@ -77,7 +76,7 @@ class Playlist extends Component {
   }
 }
 
- class App extends Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -88,7 +87,8 @@ class Playlist extends Component {
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
-
+    if (!accessToken)
+      return;
     fetch('https://api.spotify.com/v1/me', {
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response => response.json())
@@ -97,32 +97,57 @@ class Playlist extends Component {
         name: data.display_name
       }
     }))
+
     fetch('https://api.spotify.com/v1/me/playlists', {
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response => response.json())
-    .then(data => this.setState({
-      playlists: data.items.map(item => {
-        console.log(data.items)
+    .then(playlistData => {
+      let playlists = playlistData.items
+      let trackDataPromises = playlists.map(playlist => {
+        let responsePromise = fetch(playlist.tracks.href, {
+          headers: {'Authorization': 'Bearer ' + accessToken}
+        })
+        let trackDataPromise = responsePromise
+          .then(response => response.json())
+        return trackDataPromise
+      })
+      let allTracksDataPromises = 
+        Promise.all(trackDataPromises)
+      let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+        trackDatas.forEach((trackData, i) => {
+          playlists[i].trackDatas = trackData.items
+            .map(item => item.track)
+            .map(trackData => ({
+              name: trackData.name,
+              duration: trackData.duration_ms / 1000
+            }))
+        })
+        return playlists
+      })
+      return playlistsPromise
+    })
+    .then(playlists => this.setState({
+      playlists: playlists.map(item => {
         return {
           name: item.name,
           imageUrl: item.images[0].url, 
-          songs: []
+          songs: item.trackDatas.slice(0,3)
         }
     })
     }))
 
   }
-
-  
-
   render() {
     let playlistToRender = 
       this.state.user && 
       this.state.playlists 
-        ? this.state.playlists.filter(playlist => 
-          playlist.name.toLowerCase().includes(
-            this.state.filterString.toLowerCase())) 
-        : []
+        ? this.state.playlists.filter(playlist => {
+          let matchesPlaylist = playlist.name.toLowerCase().includes(
+            this.state.filterString.toLowerCase()) 
+          let matchesSong = playlist.songs.find(song => song.name.toLowerCase()
+            .includes(this.state.filterString.toLowerCase()))
+          return matchesPlaylist || matchesSong
+        }) : []
     return (
       <div className="App">
         {this.state.user ?
@@ -139,8 +164,7 @@ class Playlist extends Component {
             <Playlist playlist={playlist} />
           )}
         </div> : <button onClick={() => {
-
-    window.location = window.location.href.includes('localhost') 
+            window.location = window.location.href.includes('localhost') 
               ? 'http://localhost:8888/login' 
               : 'https://better-playlists-backend.herokuapp.com/login' }
           }
